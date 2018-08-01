8d271d847e4f75455f479a352dc36319c5198bf7
ming <sjm393429023@gmail.com>
2018-08-01 13:47:30 +0800

    Revert "Revert "SpringBoot+shiro""
    This reverts commit 1e95f9d43e530bc9d095f7277fc53933a39acb92.

---
+++ spring-shiro/src/main/java/com/example/springshiro/sevice/impl/UserInfoServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.springshiro.sevice.impl;
+
+import com.example.springshiro.dao.UserInfoDao;
+import com.example.springshiro.entity.UserInfo;
+import com.example.springshiro.sevice.UserInfoService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class UserInfoServiceImpl implements UserInfoService {
+    @Resource
+    private UserInfoDao userInfoDao;
+    @Override
+    public UserInfo findByUsername(String username) {
+        System.out.println("UserInfoServiceImpl.findByUsername()");
+        return userInfoDao.findByUsername(username);
+    }
+}
